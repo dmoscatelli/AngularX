c4901551639298de0c5450e185864534505bc193
dmoscatelli <dmoscatelli@luc.edu>
2016-02-20 13:57:25 -0600

    Initial commit of main marketing page

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,35 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require jquery-ui
+//= require jquery1.11.0.min
+//= require bootstrap.min
+//= require jquery.easing.1.3.min
+//= require modernizr.custom.min
+//= require plugins.min
+//= require jquery.superslides.min
+//= require Chart
+//= require custom
+//= require slider.js
+
+
+
+
+
+
+
+
+
+
---
+++ app/assets/javascripts/chart.js
@@ -0,0 +1,10 @@
+/* =======================================================================
+   DOUGHNUT CHART
+========================================================================== */
+
+$(document).ready(function() {
+var ctx = document.getElementById("chart2-area").getContext("2d");
+var myNewChart = new Chart(ctx).PolarArea(data);
+
+});
+
